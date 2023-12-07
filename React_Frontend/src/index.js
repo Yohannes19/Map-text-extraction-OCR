c31d072ea3ee48896d3148e32ef9a7c393645721
Yohannes19 <joeabrha@gmail.com>
2023-12-07 16:25:50 +0100

    endpoint static typo e

--- React_Frontend/src/index.js
+++ React_Frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css'
+import ReactDOM from 'react-dom';
+import './index.css';
 import { HashRouter } from 'react-router-dom';
-import App from "./App";
+import App from './App';
+
 ReactDOM.render(
-    <HashRouter>
-        <App />
-    </HashRouter>,
-    document.getElementById('root')
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
 );
