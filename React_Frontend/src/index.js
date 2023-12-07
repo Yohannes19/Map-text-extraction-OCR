b44201f4ba66dcb3db8dd33f015db89e557af3c5
Yohannes19 <joeabrha@gmail.com>
2023-12-07 16:45:34 +0100

    build front-end a

--- React_Frontend/src/index.js
+++ React_Frontend/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+//import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-);
+import App from "./App";
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App/>)
